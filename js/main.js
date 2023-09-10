7125ef77a14bb3b9fafde56087d41c27534ebfac
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-09-10 23:55:46 +0500

    add header scroll styles and scripts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const header = document.querySelector('.header');
+const headerNavLink = document.querySelector('.header__nav--link');
 const headerMenuBtn =  document.querySelector('.header__menubtn');
 const headerNav = document.querySelector('.header__nav');
 const headerMenuBtnOpen = document.querySelector('.header__menubtn--open');
@@ -6,6 +8,15 @@ const headerMenuBtnClose = document.querySelector('.header__menubtn--close');
 
 
 
+window.addEventListener('scroll', (e) => {
+    if(window.pageYOffset > 0) {
+        header.style.backgroundColor = '#81BFA8';
+        // headerNavLink.addEventLis
+    }
+    else {
+        header.style.backgroundColor = 'inherit';
+    }
+})
 
 
 headerMenuBtn.addEventListener('click', () => {
